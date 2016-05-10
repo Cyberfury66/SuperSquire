0f6eb089c1454ab0dd23a1c3bc27b6cda77dbb94
NBarber789 <nbarber789@gmail.com>
2016-05-10 10:20:28 -0700

    Used a better way of defining methods in classes
    It is now:
    
    this.methodName = function() {
        //method definition
    }
    
    i.e:
    
    this.getX = function() {
        return x;
    };

--- script/Point.js
+++ script/Point.js
@@ -1,14 +1,12 @@
-function getX() {
-    return this.x;
-}
-
-function getY() {
-    return this.y;
-}
-
 function Point(x, y) {
     this.x = x;
     this.y = y;
-    this.getX = getX;
-    this.getY = getY;
+
+    this.getX = function() {
+        return x;
+    };
+
+    this.getY = function() {
+        return y;
+    };
 }
--- script/Squire.js
+++ script/Squire.js
@@ -1,87 +1,68 @@
-function setShield(newShield) {
-    this.currentShield = newShield;
-}
-
-function move(direction) {
-    switch (direction) {
-       case 0:
-       moveUp();
-       break;
-
-       case 1:
-       moveRight();
-       break;
-
-       case 2:
-       moveDown();
-       break;
-
-       case 3:
-       moveLeft();
-       break;
-
-       default:
-       break;
-    }
-}
-
-function moveUp() {
-    with(this) {
-        if(currentCell.getY() != 0) {
-            setCell(currentCell.getX(), currentCell.getY()+1);//This will probably have to be a Game method, which means that Squire needs a reference.
-            this.position = currentCell.getCenter();
-        }
-    }
-}
-
-function moveRight() {
-    with(this) {
-        if(currentCell.getX() != 2) {
-            setCell(currentCell.getX()+1, currentCell.getY());
-            this.position = currentCell.getCenter();
-        }
-    }
-}
-
-function moveDown() {
-    with(this) {
-        if(currentCell.getY() != 2) {
-            setCell(currentCell.getX(), currentCell.getY()-1);
-            this.position = currentCell.getCenter();
-        }
-    }
-}
-
-function moveLeft() {
-    with(this) {
-        if(currentCell.getX() != 0) {
-            setCell(currentCell.getX()-1, currentCell.getY());
-            position = currentCell.getCenter();
-        }
-    }
-}
-
-function getPosition() {
-    return this.position;
-}
-
-function getShield() {
-    return this.currentShield;
-}
-
 function Squire(startingShield, startingCell, startingPosition) {
     this.currentShield = startingShield;
     this.currentCell = startingCell;
     this.position = startingPosition;
 
-    this.setShield = setShield;
+    this.setShield = function(newShield) {
+        currentShield = newShield;
+    };
 
-    this.move = move;
-    //this.moveUp = moveUp;
-    //this.moveRight = moveRight;
-    //this.moveDown = moveDown;
-    //this.moveLeft = moveLeft;
+    this.move = function(direction) {
+        switch (direction) {
+           case 0:
+           moveUp();
+           break;
 
-    this.getPosition = getPosition;
-    this.getShield = getShield;
+           case 1:
+           moveRight();
+           break;
+
+           case 2:
+           moveDown();
+           break;
+
+           case 3:
+           moveLeft();
+           break;
+
+           default:
+           break;
+        }
+    };
+
+    this.moveUp = function() {
+        if(currentCell.getY() != 0) {
+            setCell(currentCell.getX(), currentCell.getY()+1);//This will probably have to be a Game method, which means that Squire needs a reference.
+            position = currentCell.getCenter();
+        }
+    };
+
+    this.moveRight = function() {
+        if(currentCell.getX() != 2) {
+            setCell(currentCell.getX()+1, currentCell.getY());
+            position = currentCell.getCenter();
+        }
+    };
+
+    this.moveDown = function() {
+        if(currentCell.getY() != 2) {
+            setCell(currentCell.getX(), currentCell.getY()-1);
+            position = currentCell.getCenter();
+        }
+    };
+
+    this.moveLeft = function() {
+        if(currentCell.getX() != 0) {
+            setCell(currentCell.getX()-1, currentCell.getY());
+            position = currentCell.getCenter();
+        }
+    };
+
+    this.getPosition = function() {
+        return position;
+    };
+
+    this.getShield = function() {
+        return currentShield;
+    };
 }
