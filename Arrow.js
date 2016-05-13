c94d707da1beaa21f97d2155b514c5c84bf4802b
NBarber789 <nbarber789@gmail.com>
2016-05-12 19:18:39 -0700

    Polished a tiny bit
    Now the shadows growth is calculated so that they grow at a speed dependant on the arrows distance from the target.
    I.e. The shadows of arrows targeting column one grow quicker.
    Right now the max shadow size is 100. Another idea is to have the same growth rate for all shadows but have the shadows start smaller the farther right you are.
    
    Also made it so that the arrows in the middle column hit the shield by changing the 700 destination to 685. Maitiu thought that this looked weird, but having only those arrows offset from the shield looks weirder.

---
+++ Arrow.js
@@ -0,0 +1,118 @@
+function Arrow(height, stg){
+
+  this.shadowTex = PIXI.Texture.fromImage("assets\\shadow.png");
+  this.shadowImg = new PIXI.Sprite(this.shadowTex);
+
+  this.start;
+  this.end;
+  this.speed = 10;
+  this.shadowMaxSize = 100;
+
+  this.x = 0;
+  this.stage = stg;
+  this.spawned = false;
+  this.redherring = false;
+
+  this.getImg = function(){
+    return this.img;
+  }
+
+  this.setImg = function(){
+      var num = Math.floor((Math.random()*5) + 1);
+      if(num == 5) {
+          this.texture = PIXI.Texture.fromImage("assets\\fish.png");
+          this.img = new PIXI.Sprite(this.texture);
+          this.img.width = 100;
+          this.img.height = 100;
+          this.redherring = true;
+      } else {
+          this.texture = PIXI.Texture.fromImage("assets\\fireArrow.png");
+          this.img = new PIXI.Sprite(this.texture);
+          this.redherring = false;
+      }
+  }
+
+  this.isRedherring = function() {
+      return this.redherring;
+  }
+
+  this.getShadowImg = function(){
+    return this.shadowImg;
+  }
+
+  this.setStart = function(){
+    this.start = Math.floor((Math.random() * 3) + 1);
+    if(this.start == 1){
+      this.y = height/2;
+    } else if(this.start == 2){
+      this.y = height/6;
+    } else {
+      this.y = (height/6) * 5;
+    }
+  }
+
+  this.setEnd = function(){
+    this.end = Math.floor((Math.random() * 3) + 1);
+    if(this.redherring == false) {
+        if(this.end == 1){
+          this.destination = 800;
+        } else if(this.end == 2){
+          this.destination = 685;
+        } else {
+          this.destination = 570;
+        }
+    } else {
+        this.destination = 960;
+    }
+
+    this.timeTillHit = this.destination / this.speed;//calculates the time it will take to hit the target.
+
+  }
+
+  this.getX = function(){
+    return this.x;
+  }
+
+  this.getY = function(){
+    return this.y;
+  }
+
+  this.getDestination = function(){
+    return this.destination;
+  }
+
+  this.getPosition = function(){
+    return this.img.position.x;
+  }
+  this.isSpawned = function(){
+    return this.spawned;
+  }
+
+  this.move = function(){
+    if(this.redherring == true) {
+        this.img.position.x += this.speed;
+        this.img.rotation += 0.05;
+    } else {
+        this.shadowImg.width += this.shadowGrowthSpeed;//Grows by the calculated growth speed every tick.
+        this.shadowImg.height += this.shadowGrowthSpeed;
+        this.img.position.x += this.speed;
+    }
+  }
+
+  this.create = function(){
+    this.shadowImg.width = 1;
+    this.shadowImg.height = 1;
+    this.stage.addChild(this.shadowImg);
+    this.stage.addChild(this.img);
+
+    this.spawned = true;
+
+    this.shadowGrowthSpeed = this.shadowMaxSize / this.timeTillHit;//calculates how fast the shadow should grow to reach max size when the arrow hits.
+  }
+
+  this.remove = function(){
+    this.stage.removeChild(this.img);
+    this.stage.removeChild(this.shadowImg);
+    this.spawned = false;
+  }
+}
