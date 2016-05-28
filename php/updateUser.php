c3388e8a0c917dffbae1c640b19a7eb7379070ed
NBarber789 <nbarber789@gmail.com>
2016-05-27 17:22:16 -0700

    Added comments

--- php/updateUser.php
+++ php/updateUser.php
@@ -1,6 +1,9 @@
 <?php
+//PDO code sourced by from the PHP documentation website
+//http://php.net/docs.php
 $dbh = new PDO('mysql:host=localhost;dbname=SuperSquireDB', 'SuperSquireUser', 'SuperSquire2016', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'')); 
 
+//SQL statement to update the user with whatever information is posted
 $updateUser = $dbh->prepare('UPDATE User SET hiScore=?, herringClicked=?, arrowsBlocked=?, herringsSeen=? WHERE username = ?');
 $updateUser->execute(array($_POST['hiScore'], $_POST['herringClicked'], $_POST['arrowsBlocked'], $_POST['herringsSeen'], $_POST['username']));
 ?>
